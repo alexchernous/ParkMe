da74d43efcb068beab8d7337d39683c0e9d48306
unknown <alex.a.chernous@gmail.com>
2020-05-08 17:37:30 -0400

    new environment

--- app/src/main/java/asdf/parkme/MainActivity.kt
+++ app/src/main/java/asdf/parkme/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 
 //const val EXTRA_MESSAGE = "asdf.parkme.MESSAGE"
 
